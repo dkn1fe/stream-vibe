0ccd788cea9d60c2a60650c7e3f908daa412a443
dknife <dknifeb@mail.ru>
2024-06-13 20:56:56 +0300

    feat: add header & install react-router-dom

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+import { Header } from "@/components/Header/Header"
+
+export const HomePage = () => {
+    return (
+        <header>
+            <Header/>
+        </header>
+    )
+}
---
+++ src/shared/utils/burgerMenuInfo.ts
@@ -0,0 +1,8 @@
+import { Home, Clapperboard, UserCog, Bell } from "lucide-react";
+
+export const navButtonInfo = [
+  { id: 1, title: "Home", link: "/",},
+  { id: 2, title: "Movies & Shows", link: "/movies"},
+  { id: 3, title: "Support", link: "/support",},
+  { id: 4, title: "Subscriptions", link: "/subscriptions",},
+];
