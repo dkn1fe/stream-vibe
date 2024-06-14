8b9783bd04beb7718d93ce30410d6c463a63140c
dknife <dknifeb@mail.ru>
2024-06-14 20:01:18 +0300

    feat: add main-info block

---
+++ src/components/mainBlock/MainBlock.tsx
@@ -0,0 +1,10 @@
+import { Header } from "../Header/Header";
+import { MainInfo } from "./mainInfo";
+
+export const MainBlock = () => {
+  return (
+    <>
+   
+    </>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./app/App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+import './reset.css'
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
