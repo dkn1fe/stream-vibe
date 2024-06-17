1d15a344ec3c80c879644ee7ebd61d493874c747
dknife <dknifeb@mail.ru>
2024-06-17 18:20:04 +0300

    feat: add accordion-questions

---
+++ src/shared/utils/questionsList.ts
@@ -0,0 +1,12 @@
+
+
+export const questionsList = [
+    {id:1,title:'What is StreamVibe?'},
+    {id:2,title:'How much does StreamVibe cost?'},
+    {id:3,title:'What content is available on StreamVibe?'},
+    {id:4,title:'How can I watch StreamVibe?'},
+    {id:5,title:'How do I sign up for StreamVibe?'},
+    {id:6,title:'What is the StreamVibe free trial?'},
+    {id:7,title:'How do I contact StreamVibe customer support?'},
+    {id:8,title:'What are the StreamVibe payment methods?'}
+]
