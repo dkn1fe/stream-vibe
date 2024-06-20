e0bcfa2e05a998dd96b3fd7dc3827196a6fbd55c
dknife <dknifeb@mail.ru>
2024-06-20 18:44:04 +0300

    feat:add arhitecture movies & show page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,12 @@
 import { HomePage } from "@/pages/HomePage";
+import { MoviesShowPage } from "@/pages/MoviesShowPage";
+import { Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/movies&show" element={<MoviesShowPage />} />
+    </Routes>
   );
 }
---
+++ src/pages/MoviesShowPage.tsx
@@ -0,0 +1,11 @@
+import { Header } from "@/components/Header/Header";
+
+export const MoviesShowPage = () => {
+  return (
+    <div className="container">
+      <header>
+        <Header />
+      </header>
+    </div>
+  );
+};
