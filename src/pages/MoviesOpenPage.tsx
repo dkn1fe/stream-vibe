2063fbcf26b96398e944a2cede1f040bcdda12bc
dknife <dknifeb@mail.ru>
2024-06-29 18:48:07 +0300

    feat:add spinner in movies open page

--- src/pages/MoviesOpenPage.tsx
+++ src/pages/MoviesOpenPage.tsx
@@ -1,17 +1,21 @@
-import Footer from "@/components/Footer/Footer";
 import { Header } from "@/components/Header/Header";
 import { MoviesBanner } from "@/components/MoviesOpenPage/moviesBanner/MoviesBanner";
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { onGetMoviesById, onGetStaffById } from "@/shared/api/moviesApiById";
-import { AppDispatch } from "@/app/store/store";
+import { AppDispatch, RootState } from "@/app/store/store";
 import { MoviesInfo } from "@/components/MoviesOpenPage/moviesInfo/MoviesInfo";
 import PlanBanner from "@/components/PlanBanner/PlanBanner";
+import Spinner from "@/components/spinner/Spinner";
+import Footer from "@/components/Footer/Footer";
 
 export const MoviesOpenPage = () => {
   const { moviesId } = useParams();
   const dispatch = useDispatch<AppDispatch>();
+  const { moviesInfoStatus } = useSelector(
+    (state: RootState) => state.moviesPageSlice
+  );
 
   useEffect(() => {
     dispatch(onGetMoviesById(moviesId));
@@ -24,13 +28,17 @@ export const MoviesOpenPage = () => {
         <header>
           <Header />
         </header>
-        {}
-        <main className="py-20">
-          <MoviesBanner />
-        </main>
-        <section className="pb-10">
-          <MoviesInfo />
-        </section>
+        {moviesInfoStatus === "loading" && <Spinner />}
+        {moviesInfoStatus === "idle" && (
+          <>
+            <main className="py-20">
+              <MoviesBanner />
+            </main>
+            <section className="pb-10">
+              <MoviesInfo />
+            </section>
+          </>
+        )}
         <section className="py-20">
           <PlanBanner />
         </section>
